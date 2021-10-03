f517f92fb316f1146915bb68beb4ef1c13d33b13
TioMarc0 <marcomariobros68@gmail.com>
2021-10-02 21:50:56 -0300

    Update ano.java

--- exercícios/src/ano.java
+++ exercícios/src/ano.java
@@ -1,9 +1,31 @@
-
+import java.util.Scanner;
 public class ano {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		Scanner ent = new Scanner(System.in);
+		int Ano;
+		
+		System.out.print("coloque o ano: ");
+		Ano = ent.nextInt();
+		
+		if (Ano % 4 == 0) {
+		System.out.println("o ano é bissexto: ");
+		}else if ((Ano %4 == 0)&&(Ano % 100 != 0)){
+			System.out.println("O ano não é bissexto: ");
+			}else {
+				System.out.println("não é bissexto "+ Ano);
+		
+		}
+		
+		
+		ent.close();
+		}
 
 }
+
+	
+		
+		
+
+
