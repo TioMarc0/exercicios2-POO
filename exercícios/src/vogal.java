8418e2963b9eb3424107806687127b3e4bc51828
TioMarc0 <marcomariobros68@gmail.com>
2021-10-02 23:09:30 -0300

    Create vogal.java

---
+++ exercícios/src/vogal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class vogal {
+
+	public static void main(String[] args) {
+
+		// TODO Auto-generated method stub
+		Scanner ent = new Scanner(System.in);
+		System.out.println("escreva uma das letras ");
+		char[] vogal = new char[] { 'A' };
+		for(char reader : vogal) {}
+		
+
+	}
+
+}
